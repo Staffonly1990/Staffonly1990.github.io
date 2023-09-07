5aced223447092b748833776af1b4a722caf1dd3
sergey.demin <sergey.demin@shtormtech.ru>
2023-09-07 16:36:23 +0300

    start

--- src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-
-import type { RootState, AppDispatch } from './index';
-
-export const useMathDispatch = () => useDispatch<AppDispatch>();
-export const useMathSelector: TypedUseSelectorHook<RootState> = useSelector;
